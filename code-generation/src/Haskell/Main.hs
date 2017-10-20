e7d17d6757777da0627f54158a6c16fb3a69a3d8
Coleman <gibsonjc@rose-hulman.edu>
2017-10-19 21:09:34 -0400

    Main takes a path

--- code-generation/src/Haskell/Main.hs
+++ code-generation/src/Haskell/Main.hs
@@ -1,15 +1,12 @@
 module Main where
 
-import CodeGeneration.Generation
 import CodeGeneration.EvalFile
-import Scheme.Types
 
-import Control.Monad
 import System.Environment
 
 main :: IO ()
 main = do
-  path <- tail <$> getArgs
-  let ps = displayProgram <$> generatePrograms
-  results <- join <$> mapM getResults ps
+  path <- head <$> getArgs
+  ps <- readFile path
+  results <- getResults ps
   putStrLn $ writeResults results
